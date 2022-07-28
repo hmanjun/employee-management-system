670e8911332ef37cee54f05757b2d07b17356253
hmanjun <harshith.hm03@gmail.com>
2022-07-28 11:29:30 -0700

    Add few seed data points

--- db/schema.sql
+++ db/schema.sql
@@ -2,25 +2,25 @@ DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 USE company_db;
 
-CREATE TABLE departments(
+CREATE TABLE department(
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 )
 
-CREATE TABLE roles(
+CREATE TABLE role(
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES departments (id) ON DELETE SET NULL--Remove set null in future
+    FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE SET NULL--Remove set null in future
 )
 
-CREATE TABLE employees(
+CREATE TABLE employee(
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES roles (id),
-    FOREIGN KEY (manager_id) REFERENCES employees (id)
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
 )
---
+++ db/seed.sql
@@ -0,0 +1,16 @@
+USE company_db;
+INSERT INTO department(name)
+VALUES ("Engineering"),
+       ("Finance"),
+       ("Legal"),
+       ("Sales");
+
+INSERT INTO role(title, salary, department_id)
+VALUES ("Sales Lead", 100000, 4),
+       ("Lead Engineer", 150000, 1),
+       ("Account Manager", 160000, 2),
+       ("Lawyer", 190000, 3);
+
+INSERT INTO employee(first_name,last_name,role_id,manager_id)
+VALUES ("John", "Doe", 1, null),
+       ("Mike","Chan",1,1);
