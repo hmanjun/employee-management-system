d1c053a1317b14da50150e78304460c083ce62ca
hmanjun <harshith.hm03@gmail.com>
2022-07-28 20:11:39 -0700

    Fix displaying all employees to show manager name instead of id

--- index.js
+++ index.js
@@ -235,11 +235,14 @@ const addDepartment = () => {
 const viewEmployees = () => {
     return new Promise(resolve => {
         db.query(`
-        SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, employee.manager_id AS manager
+        SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name," ",m.last_name) AS manager
         FROM employee 
         JOIN role on employee.role_id = role.id
-        JOIN department on role.department_id = department.id`,(err,result) => {
+        JOIN department on role.department_id = department.id
+        LEFT JOIN employee m on m.id = employee.manager_id`,(err,result) => {
+            console.log('\n')
             err ? console.log(err) : console.table(result)
+            console.log('\n')
             resolve("resolve")
         })
     })
